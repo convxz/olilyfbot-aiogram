ae185c58a4663273524f5ba455abad80e7868c8f
convxz <m.kadzaev77@gmail.com>
2023-02-25 12:44:11 +0300

    upgrade module 'db_help'

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
-""" my first bot in telegram """
-
 from os import getenv
-from sqlite3 import connect, Connection, Cursor
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
-from db_help import check_user_in_table, db, sql
+from db_help import check_user_in_table, append_users, check_ingame
+from db_help import change_ingame
 
 load_dotenv()
 BOT_TOKEN: str = str(getenv("BOT_TOKEN"))
@@ -13,22 +11,47 @@ bot: Bot = Bot(token=BOT_TOKEN)
 dp: Dispatcher = Dispatcher(bot)
 
 
+async def process_start_command(message: types.Message) -> None:
+    # This func will reply to command 'start'
+    id = message.from_id
+
+    if not(check_user_in_table(id)):
+        append_users(message)
+
+    if not(check_ingame(id)):
+        await message.answer("Hello!\n\nThis bot was created by kartosha, you can give him motivation to work so much more:\n\n2202205347399922")
+    else:
+        await message.answer("you are in game.")
+
+
 async def process_echo_message(message: types.Message) -> None:
     # This func will send you your text message
+    id = message.from_id
+    
+    if check_ingame(id):
+        await message.answer("you are in game.")
     await message.answer(message.text)
 
 
-async def process_start_command(message: types.Message) -> None:
-    # This func will reply to command 'start'
+async def process_start_game(message: types.Message) -> None:
+    if check_ingame(message.from_id):
+        await message.answer("you are in game.")
+    else:
+        await message.answer("alright, welcome to game!")
+        change_ingame(message.from_id)
 
-    if not(check_user_in_table(message.from_id)):
-        sql.execute(f"INSERT INTO users (username, id, ingame) values (?, ?, ?)", (message.from_user.username, message.from_user.id, 1))
-        db.commit()
 
-    await message.answer("Hello!\n\nThis bot was created by kartosha, you can give him motivation to work so much more:\n\n2202205347399922")
+async def process_cancel_game(message: types.Message) -> None:
+    if check_ingame(message.from_id):
+        await message.answer("you are not in game already.")
+        change_ingame(message.from_id)
+    else:
+        await message.answer("you are not in game.")
 
 
 if __name__ == "__main__":
     dp.register_message_handler(process_start_command, commands=["start"])
+    dp.register_message_handler(process_start_game, commands=["start_game"])
+    dp.register_message_handler(process_cancel_game, commands=["cancel"])
     dp.register_message_handler(process_echo_message, content_types=["text"])
     executor.start_polling(dp, skip_updates=True)
--- db_help.py
+++ db_help.py
@@ -1,4 +1,6 @@
 from sqlite3 import connect, Connection, Cursor
+from random import randint
+from aiogram import types
 
 db: Connection = connect('server.db')
 sql: Cursor = db.cursor()
@@ -21,12 +23,65 @@ db.commit()
 
 
 def check_user_in_table(id: int) -> bool:
-    sql.execute("SELECT * FROM users;")
-    users = sql.fetchall()
+    """
+    this func will return true if user in users else false
+    id: int - user id
+    return: bool - true if user in users else false
+    """
+    sql.execute(f"SELECT * FROM users WHERE id = {id};")
+    user = sql.fetchone()
 
-    for user in users:
-        if user[1] == id:
+    if user is None:
+        return False
+    return True
+
+
+def append_users(message: types.Message) -> None:
+    """
+    this func will add user to users table
+    username: str - user name
+    id: int - user id
+    ingame: int - 0 if not in game else 1
+    return: None
+    """
+    n = randint(0, 100)
+    sql.execute("INSERT INTO users (username, id, ingame) values (?, ?, ?)", (message.from_user.username, message.from_id, 0))
+    sql.execute("INSERT INTO users_stats (id, attempts, games_count, games_win, rand_num) values (?, ?, ?, ?, ?)", (message.from_id, 7, 0, 0, n))
+    db.commit()
+
+
+def change_ingame(id: int) -> None:
+    if check_ingame(id):
+        with db:
+            sql.execute(f"UPDATE users SET ingame = 0 WHERE id = {id}")
+    else:
+        with db:
+            sql.execute(f"UPDATE users SET ingame = 1 WHERE id = {id}")
+
+
+    
+
+def check_ingame(id: int) -> bool:
+    sql.execute(f"SELECT * FROM users WHERE id = {id}")
+    user = sql.fetchone()
+
+    if user:
+        if user[2]:
             return True
-            break
     return False
 
+
+def tests():
+    id = 2120080409
+
+    sql.execute(f"SELECT * FROM users WHERE id = {id}")
+
+    users = sql.fetchone()
+    print(users)
+
+    for user in users:
+        print(user)
+
+
+if __name__ == "__main__":
+    tests()
